f6f284a7f9813a7ea07542f604caf2b8834fe2db
zhangzhuojia <zhangzhuojia2167@dingtalk.com>
2018-11-26 17:22:18 +0800

    add animation

--- app/src/main/java/cn/kc/em/frag/common/FragmentFactory.java
+++ app/src/main/java/cn/kc/em/frag/common/FragmentFactory.java
@@ -3,6 +3,9 @@ package cn.kc.em.frag.common;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import cn.kc.em.MainActivity;
 import cn.kc.em.R;
 import cn.kc.em.frag.FirstFragment;
@@ -18,11 +21,41 @@ import cn.kc.em.frag.ThirdFragment;
  */
 public class FragmentFactory {
 
-    private static String[] NAMES = {FirstFragment.class.getName(), SecondFragment.class.getName(),
-            ThirdFragment.class.getName(), FourthFragment.class.getName()};
+    private static LinkedList<String> NAMES = new LinkedList<String>() {
+        {
+            add(0, FirstFragment.class.getName());
+            add(1, SecondFragment.class.getName());
+            add(2, ThirdFragment.class.getName());
+            add(3, FourthFragment.class.getName());
+        }
+    };
+
+    private static int showIndex = -1;
+
 
     public static void show(MainActivity context, String clzName) {
         FragmentManager supportFragmentManager = context.getSupportFragmentManager();
+
+
+        int currentIndex = NAMES.indexOf(clzName);
+
+        if (currentIndex == showIndex) {
+            return;
+        }
+
+        int enter = -1;
+        int exit = -1;
+
+        if (showIndex < currentIndex) {
+            //如果上一个页面的下标小于要显示的页面下标，动画从左向右
+            enter = R.anim.enter_from_right;
+            exit = R.anim.exit_to_left;
+        } else if (showIndex > currentIndex) {
+            //如果上一个页面的下标大于要显示的页面下标，动画从右向左
+            enter = R.anim.enter_from_left;
+            exit = R.anim.exit_to_right;
+        }
+
 
         //隐藏非目标fragment
         for (String name :
@@ -30,7 +63,7 @@ public class FragmentFactory {
             Fragment f = supportFragmentManager.findFragmentByTag(name);
             if (!name.equalsIgnoreCase(clzName)) {
                 if (f != null)
-                    supportFragmentManager.beginTransaction().hide(f).commit();
+                    supportFragmentManager.beginTransaction().setCustomAnimations(enter, exit).hide(f).commit();
             }
         }
 
@@ -40,7 +73,7 @@ public class FragmentFactory {
         if (fragmentByTag == null) {
             try {
                 fragmentByTag = (Fragment) Class.forName(clzName).newInstance();
-                supportFragmentManager.beginTransaction().add(R.id.frame, fragmentByTag, clzName).commit();
+                supportFragmentManager.beginTransaction().setCustomAnimations(enter, exit).add(R.id.frame, fragmentByTag, clzName).commit();
             } catch (InstantiationException e) {
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
@@ -49,8 +82,9 @@ public class FragmentFactory {
                 e.printStackTrace();
             }
         } else {
-            supportFragmentManager.beginTransaction().show(fragmentByTag).commit();
+            supportFragmentManager.beginTransaction().setCustomAnimations(enter, exit).show(fragmentByTag).commit();
         }
 
+        showIndex = NAMES.indexOf(clzName);
     }
 }
